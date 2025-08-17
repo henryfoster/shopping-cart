aab85f02a3e387b60c270cc49183fc4afb378b17
HenryFoster <andreev.1.eduard@gmail.com>
2025-08-18 01:07:09 +0200

    added generic exception listener

---
+++ src/EventListener/GenericExceptionListener.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventListener;
+
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+final class GenericExceptionListener implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+    public const string MESSAGE = 'Internal Server Error';
+
+    #[AsEventListener(priority: -100)]
+    public function onExceptionEvent(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $pathInfo = $event->getRequest()->getPathInfo();
+
+        $this->logger->error($exception->getMessage(), [
+            'exception' => $exception,
+        ]);
+
+        if (str_starts_with($pathInfo, '/api/') && !$event->hasResponse()) {
+            $response = new JsonResponse(
+                data: [
+                    'error' => self::MESSAGE,
+                ],
+                status: Response::HTTP_INTERNAL_SERVER_ERROR,
+                headers: [
+                    'Content-Type' => 'application/json',
+                ]
+            );
+            $event->setResponse($response);
+        }
+    }
+}
