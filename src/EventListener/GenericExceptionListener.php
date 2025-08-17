a79f9f15679082bbac62c2dbf080d64640fcc144
HenryFoster <andreev.1.eduard@gmail.com>
2025-08-18 01:10:54 +0200

    fix phpstan issue

--- src/EventListener/GenericExceptionListener.php
+++ src/EventListener/GenericExceptionListener.php
@@ -4,16 +4,21 @@ namespace App\EventListener;
 
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
-final class GenericExceptionListener implements LoggerAwareInterface
+final class GenericExceptionListener
 {
-    use LoggerAwareTrait;
     public const string MESSAGE = 'Internal Server Error';
+
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {
+    }
 
     #[AsEventListener(priority: -100)]
     public function onExceptionEvent(ExceptionEvent $event): void
